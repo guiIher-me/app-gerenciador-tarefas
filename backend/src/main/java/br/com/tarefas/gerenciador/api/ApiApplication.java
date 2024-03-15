836d8fa8d7e51c5478a442b93ffee3b64c04f14b
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2024-03-15 00:19:32 -0300

    feat: add base project

---
+++ backend/src/main/java/br/com/tarefas/gerenciador/api/ApiApplication.java
@@ -0,0 +1,27 @@
+package br.com.tarefas.gerenciador.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EntityScan(basePackages = "br.com.tarefas.gerenciador.model")
+@ComponentScan(basePackages = "br.*")
+@EnableJpaRepositories(basePackages = "br.com.tarefas.gerenciador.repository")
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@AutoConfiguration
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/br/com/tarefas/gerenciador/model/User.java
@@ -0,0 +1,49 @@
+package br.com.tarefas.gerenciador.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    protected String login;
+
+    protected String password;
+
+    protected String name;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
