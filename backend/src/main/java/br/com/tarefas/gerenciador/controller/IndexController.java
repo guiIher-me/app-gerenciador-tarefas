c087ae8cb75d3774aea0977a82b047d3d7afeb7f
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2024-03-24 15:03:28 -0300

    feat: add index controller

--- backend/src/main/java/br/com/tarefas/gerenciador/controller/IndexController.java
+++ backend/src/main/java/br/com/tarefas/gerenciador/controller/IndexController.java
@@ -1,7 +1,16 @@
 package br.com.tarefas.gerenciador.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IndexController {
 
-    
+    @GetMapping(value = "", produces = "application/json")
+    public ResponseEntity<Void> init() {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
