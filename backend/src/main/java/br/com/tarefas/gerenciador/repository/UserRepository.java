bf5a23cf9b83f7dada7ce5506555f693a9c41e2d
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2024-03-26 21:06:53 -0300

    feat: add jwt authentication

--- backend/src/main/java/br/com/tarefas/gerenciador/repository/UserRepository.java
+++ backend/src/main/java/br/com/tarefas/gerenciador/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package br.com.tarefas.gerenciador.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.tarefas.gerenciador.model.User;
