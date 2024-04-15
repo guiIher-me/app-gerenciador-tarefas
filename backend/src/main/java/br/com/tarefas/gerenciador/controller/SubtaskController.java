ac24ca1a08152739127c08b2f06d68824af587f9
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2024-04-15 13:34:49 -0300

    refactor: remove duplicated subtask endpoints

--- backend/src/main/java/br/com/tarefas/gerenciador/controller/SubtaskController.java
+++ backend/src/main/java/br/com/tarefas/gerenciador/controller/SubtaskController.java
@@ -5,11 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +16,6 @@ import br.com.tarefas.gerenciador.dto.subtask.CreateSubtaskDTO;
 import br.com.tarefas.gerenciador.dto.subtask.GetSubtaskDTO;
 import br.com.tarefas.gerenciador.model.Subtask;
 import br.com.tarefas.gerenciador.service.SubtaskService;
-import br.com.tarefas.gerenciador.service.TaskService;
 
 @RestController
 @RequestMapping("/subtask")
@@ -26,9 +23,6 @@ public class SubtaskController {
    
     @Autowired
     private SubtaskService subtaskService;
-
-    @Autowired
-    private TaskService taskService;
 
     @PostMapping(value="/", produces="application/json")
     public ResponseEntity<GetSubtaskDTO> create(@NonNull @Validated @RequestBody CreateSubtaskDTO createSubtaskDTO) {
@@ -42,22 +36,6 @@ public class SubtaskController {
         Subtask subtask = subtaskService.getOrFail(id);
         GetSubtaskDTO subtaskDTO = new GetSubtaskDTO(subtask);
         return ResponseEntity.ok().body(subtaskDTO);
-    }
-
-    @PutMapping(value="/{subtaskId}/move-to-list/{listId}", produces="application/json")
-    public ResponseEntity<GetSubtaskDTO> moveTaskToList(
-        @NonNull @Validated @PathVariable Long subtaskId,
-        @NonNull @Validated @PathVariable Long listId) {
-
-        Subtask updatedSubtask = (Subtask) taskService.moveTaskToList(subtaskId, listId);
-        GetSubtaskDTO subtaskDTO = new GetSubtaskDTO(updatedSubtask);
-        return ResponseEntity.ok().body(subtaskDTO);
-    }
-
-    @DeleteMapping(value = "/{id}", produces = "application/json")
-    public ResponseEntity<Void> deleteById(@PathVariable("id") @NonNull Long id) {
-        taskService.deleteById(id);
-        return ResponseEntity.noContent().build();
     }
     
 }
