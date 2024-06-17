f0624dc3840617af56c6da7c5f2b9084df48d326
Guilherme Cardoso <coliveira.guilherme@gmail.com>
2024-06-16 23:32:18 -0300

    refactor: centralize http classes

---
+++ frontend/src/http/index.js
@@ -0,0 +1,5 @@
+import { HttpAuthToken } from "./HttpAuthToken";
+import { HttpRequest } from "./HttpRequest";
+import { HttpRequestAuth } from "./HttpRequestAuth";
+
+export { HttpAuthToken, HttpRequest, HttpRequestAuth };
